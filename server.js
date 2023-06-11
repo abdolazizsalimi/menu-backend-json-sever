72196ba138cef96b8d2ff27c8338322fcf4f936d
sajjad hayati <sajjaddg@gmail.com>
2023-06-11 12:04:09 +0330

    fix it

--- server.js
+++ server.js
@@ -7,13 +7,7 @@ const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
-);
+
 server.use(router);
 // Listen to port
 server.listen(3000, () => {
